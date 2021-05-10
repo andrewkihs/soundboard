6e4c13c486dc8987858782c50b79a4598e94e7c4
Andrew Kihs <ajkihs@gmail.com>
2021-05-10 12:16:15 -0400

    Add basic routing, model, and controller for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  Rails.application.routes.draw do
+  namespace :api, defaults: {format: :json} do
+    resource :user, only: [:create, :show, :update]
+    resource :session, only: [:create, :destroy, :show]
+  end
+
+  root "static_pages#root"
+end
+
 end
