ea9918511256e41840ad761f00b081f9582ea0da
Andrew Kihs <ajkihs@gmail.com>
2021-05-11 11:14:55 -0400

    Add heroku

---
+++ frontend/components/discover/discover_container.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class DiscoverContainer extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return(
+      <>Discover Container</>
+    )
+  }
+}
+
+export default DiscoverContainer
