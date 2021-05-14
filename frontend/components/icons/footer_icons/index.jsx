5415e9e1556f3e0b4185e81b1693dcdb72ebf91d
Andrew Kihs <ajkihs@gmail.com>
2021-05-14 17:22:19 -0400

    Add icon svg's

--- frontend/components/icons/footer_icons/index.jsx
+++ frontend/components/icons/footer_icons/index.jsx
@@ -1,1 +1,107 @@
 import React from 'react'
+
+export const VolumeButton = (props) => {
+<svg 
+xmlns="http://www.w3.org/2000/svg" 
+width="24" 
+height="24"
+{...props}>
+  <path fill="#333" 
+  d="M4 9h4.002L12 5v14c-2.446-2.667-3.778-4-3.998-4H4V9zm10 4a1 1 0 0 0 0-2V9a3 3 0 0 1 0 6v-2zm0 4a5 5 0 0 0 0-10V5a7 7 0 0 1 0 14v-2z"/>
+</svg>
+}
+
+export const PlayButton = (props) => {
+  <svg 
+  xmlns="http://www.w3.org/2000/svg" 
+  width="24" 
+  height="24"
+  {...props}>
+    <path fill="#333" d="M8 5v14l11-7z"/></svg>
+
+}
+
+
+export const BackButton = (props) => {
+  <svg 
+  xmlns="http://www.w3.org/2000/svg" 
+  width="24" 
+  height="24"
+  {...props}>
+    <path 
+    fill="#333" 
+    d="M7 6h2v12H7V6zm2 6l8 6V6l-8 6z"
+    />
+  </svg>
+}
+
+export const NextButton = (props) => {
+  <svg 
+  xmlns="http://www.w3.org/2000/svg" 
+  width="24" 
+  height="24"
+  {...props}>
+    <path 
+    fill="#333" 
+    d="M7 18l8-6-8-6v12zm8-12v12h2V6h-2z"/>
+    </svg>
+
+}
+export const LikeButton = (props) => {
+
+  <svg 
+  width="16px" 
+  height="16px" 
+  viewBox="0 0 16 16" 
+  version="1.1" 
+  {...props}>
+
+    <title>stats_likes_grey</title>
+    <desc>Created with Sketch.</desc>
+    <defs/>
+    <g id="Page-1" 
+    stroke="none" 
+    strokeWidth="1" 
+    fill="none" 
+    fillRule="evenodd">
+        <path 
+        d="M10.8049818,3 C8.78471579,3 8.00065285,5.34486486 8.00065285,5.34486486 C8.00065285,5.34486486 7.21296387,3 5.19604494,3 C3.49431318,3 1.748374,4.09592694 2.03008996,6.51430532 C2.37372765,9.46673775 7.75491917,12.9928738 7.99310958,13.0010557 C8.23129998,13.0092378 13.7309828,9.2785378 13.981459,6.5012405 C14.1878647,4.20097023 12.5067136,3 10.8049818,3 Z" id="Imported-Layers" fill="rgb(34, 34, 34)" s/>
+    </g>
+</svg>
+
+}
+export const FollowButton = (props) => {
+  <svg 
+  xmlns="http://www.w3.org/2000/svg" 
+  width="24" 
+  height="24"
+  {...props}>
+    <path 
+    fill="#333" fillRule="nonzero" 
+    d="M10.5 5c3.25 0 3.944 3.236 2.5 7 3.717 1.534 4 4.85 4 6H4c0-1.15.283-4.466 4-6-1.445-3.775-.75-7 2.5-7zM18 4a1 1 0 011 1v1h1a1 1 0 010 2h-1v1a1 1 0 01-2 0V7.999L16 8a1 1 0 010-2l1-.001V5a1 1 0 011-1z"/>
+    </svg>
+
+}
+
+export const AfterLikeButton = (props) => {
+<svg 
+width="16px" 
+height="16px" 
+viewBox="0 0 16 16" 
+version="1.1" 
+{...props}>
+    <title>stats_likes_orange</title>
+    <desc>Created with Sketch.</desc>
+    <defs/>
+    <g 
+    id="Page-1" 
+    stroke="none" 
+    strokeWidth="1" 
+    fill="none" 
+    fillRule="evenodd" >
+        <path d="M10.8049818,3 C8.78471579,3 8.00065285,5.34486486 8.00065285,5.34486486 C8.00065285,5.34486486 7.21296387,3 5.19604494,3 C3.49431318,3 1.748374,4.09592694 2.03008996,6.51430532 C2.37372765,9.46673775 7.75491917,12.9928738 7.99310958,13.0010557 C8.23129998,13.0092378 13.7309828,9.2785378 13.981459,6.5012405 C14.1878647,4.20097023 12.5067136,3 10.8049818,3 Z" id="Imported-Layers" fill="rgb(255, 85, 0)" />
+    </g>
+</svg>
+
+}
+
--- frontend/components/icons/index.jsx
+++ frontend/components/icons/index.jsx
@@ -1,1 +1,3 @@
 export { SearchIcon, NotifBell, Messages, MoreOptions } from './header_icons'
+
+export { VolumeButton, PlayButton, BackButton, NextButton, LikeButton} from './footer_icons'
