bc62c7ce71837a01faf48257a2c4f75a4bf2f00c
Andrew Kihs <ajkihs@gmail.com>
2021-07-09 13:57:12 -0400

    Styling refactor of library component

--- frontend/components/library/library.jsx
+++ frontend/components/library/library.jsx
@@ -5,29 +5,20 @@ class Library extends React.Component{
   constructor(props){
     super(props)
   }
-
-  filterLikes(obj){
-    let res = {}  
-    const toArr = Object.entries(obj)
-  }
-
+  
   render() {
     const { songs , currentUser} = this.props
-    if (!currentUser.likes){
-      return(
-        <>You have no likes! Start liking some songs to populate this page.</>
-      )
-    }
-    if (Object.keys(songs).length===0){
-      return null
-    } else {
+    if (!currentUser.likes){ return <>You have no likes! Start liking some songs to populate this page.</> }
+    if (Object.keys(songs).length===0){ return null } 
+    else {
       return(
         <>
           <div className="grid-header">
-            <h1>All your likes in one place.</h1>
+            <h1>
+              All your likes in one place.
+            </h1>
           </div>
           <ul className="stream-ul">
-            
             {Object.keys(currentUser.likes).map((key, i) =>{
               return <SongIndexItemContainer key={i} songId={parseInt(key)}/>
             })}
