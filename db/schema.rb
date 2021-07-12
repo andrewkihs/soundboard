b1df63abd442d01cf4df5df156b0b042c62cf5d6
Andrew Kihs <ajkihs@gmail.com>
2021-07-12 16:56:38 -0400

    Add mcolumn for audio peaks to song

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_18_183553) do
+ActiveRecord::Schema.define(version: 2021_07_12_205208) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 2021_05_18_183553) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "genre"
+    t.text "audio_peaks"
     t.index ["artist_id"], name: "index_songs_on_artist_id"
     t.index ["title"], name: "index_songs_on_title"
   end
